28cbdfc5853a85f300ef7eaa82c54d0fc9873c3f
Grisha <guruwolf8@gmail.com>
2023-06-04 17:29:35 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -55,7 +55,15 @@ public class Main {
         }
         System.out.println();
     }
-    public static void task4() {
 
+    public static void task4() {
+        System.out.println("Задача 4.");
+        int[] array = {1, 2, 3};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i]++;
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
