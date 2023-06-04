1b9ed51f6ac9d783c3589111cffc29d94b5626a7
Grisha <guruwolf8@gmail.com>
2023-06-04 14:18:26 +0300

    Задача1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+
+    }
+    public static void task1(){
+        System.out.println("Задача1.");
+        int[] array=new int[]{1,2,3};
+        double[] arrayDouble=new double[]{1.57,7.654,9.986};
+        String[] arrayString=new String[]{"Привет,"," как ","дела?"};
+        System.out.println(arrayString[0]+arrayString[1]+arrayString[2]);
+
+    }
+}
